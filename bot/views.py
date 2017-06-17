d4e5383344846362cf6f735b95fcfb86334b8e1e
Luís Felipe <lfa.luisfelipe@gmail.com>
2017-06-17 16:46:22 -0300

    Views

--- bot/views.py
+++ bot/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import RedirectView, View, UpdateView, ListView, DetailView, DeleteView, CreateView
+from pprint import pprint
 from .models import Facebook 
 
 def index(request):
@@ -9,10 +10,17 @@ def index(request):
 class Bot(View):
 	def get(self, request, *args, **kwargs):
 		facebook = Facebook.objects.all().reverse()[0]
-		
 		if self.request.GET['hub.verify_token'] == facebook.verify_token:
 			return HttpResponse(self.request.GET['hub.challenge'])
 		else:
 			return HttpResponse('Error, invalid token')
-		
-		return HttpResponse('Error, invalid token')
+
+	 @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        # Converts the text payload into a python dictionary
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+		pprint (incoming_message)
+        
