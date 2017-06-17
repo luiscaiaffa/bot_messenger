60d7369e0cb2749cbb782110a52b78098247762d
Luís Felipe <lfa.luisfelipe@gmail.com>
2017-06-17 16:49:03 -0300

    Views

--- bot/views.py
+++ bot/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import RedirectView, View, UpdateView, ListView, DetailView, DeleteView, CreateView
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from pprint import pprint
 from .models import Facebook 
 
@@ -15,12 +17,12 @@ class Bot(View):
 		else:
 			return HttpResponse('Error, invalid token')
 
-	 @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return generic.View.dispatch(self, request, *args, **kwargs)
+	@method_decorator(csrf_exempt)
+	def dispatch(self, request, *args, **kwargs):
+		return generic.View.dispatch(self, request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        # Converts the text payload into a python dictionary
-        incoming_message = json.loads(self.request.body.decode('utf-8'))
+	def post(self, request, *args, **kwargs):
+		# Converts the text payload into a python dictionary
+		incoming_message = json.loads(self.request.body.decode('utf-8'))
 		pprint (incoming_message)
-        
+
